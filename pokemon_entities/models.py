ad2c8309e9bee9383d2f0f7b122fb7e82eb4441a
Alex <48862843+LobanAB@users.noreply.github.com>
2022-03-21 18:23:54 +0300

    add description to pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -18,6 +19,7 @@ class PokemonEntity(models.Model):
     strength = models.IntegerField()
     defence = models.IntegerField()
     stamina = models.IntegerField()
+
 
     def __str__(self):
         return self.Pokemon.title
