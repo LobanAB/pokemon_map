0be74ee3031bbd4476149626bc68718a49ab11d7
Alex <48862843+LobanAB@users.noreply.github.com>
2022-03-21 18:30:45 +0300

    add eng and jp pokemon title

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,8 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images', null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    title_en = models.CharField(max_length=200, null=True, blank=True)
+    title_jp = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.title
